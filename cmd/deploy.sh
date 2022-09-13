442df6b434f878f9052911d3ff5a3d65d2ff4a3c
xunmao <xunmao1989@gmail.com>
2022-09-14 00:43:54 +0800

    优化代码

--- cmd/deploy.sh
+++ cmd/deploy.sh
@@ -1,4 +1,10 @@
 #!/bin/zsh
 
+thisProject="/Users/qing/programming/java/spring-mvc"
+tomcatWebapps="/Users/qing/programming/java/tools/apache-tomcat-9.0.65/webapps/"
+
+# 通过 Maven 进行打包
+mvn package -f "$thisProject/pom.xml"
+
 # 将 war 包拷贝到 {tomcat-installation-directory}/webapps 路径下
-cp /Users/qing/programming/java/spring-mvc/target/spring-mvc.war /Users/qing/programming/java/tools/apache-tomcat-9.0.65/webapps/
+cp "$thisProject/target/spring-mvc.war" $tomcatWebapps
---
+++ src/main/java/com/xunmao/demo/controller/HelloController.java
@@ -0,0 +1,35 @@
+package com.xunmao.demo.controller;
+
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class HelloController implements Controller {
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+
+        // 1. 创建 ModelAndView 的实例，最后返回该实例
+        ModelAndView modelAndView = new ModelAndView();
+
+        // 2. 根据业务逻辑，将数据放入 ModelAndView 的实例中
+        // 2.1. 获取参数 user
+        String user = req.getParameter("user");
+        // 2.2. 设置 title
+        modelAndView.addObject("user", user);
+        // 2.2. 设置 h1
+        Date currentTime = new Date(System.currentTimeMillis());
+        String message = String.format("Hi, %s! Current Time is %s", user, currentTime);
+        modelAndView.addObject("message", message);
+
+        // 3. 视图跳转
+        modelAndView.setViewName("hello");
+
+        return modelAndView;
+    }
+
+}
