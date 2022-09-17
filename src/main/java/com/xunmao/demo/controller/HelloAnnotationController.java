40ab52318cb75cd169dcdd97e786ad443d32bd11
xunmao <xunmao1989@gmail.com>
2022-09-18 00:35:19 +0800

    优化代码

--- src/main/java/com/xunmao/demo/controller/HelloAnnotationController.java
+++ src/main/java/com/xunmao/demo/controller/HelloAnnotationController.java
@@ -7,7 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HelloAnnotationHandler {
+public class HelloAnnotationController {
 
     @RequestMapping("/hello")
     public String hello(String user, Model model) {
--- src/main/java/com/xunmao/demo/controller/HelloInterfaceController.java
+++ src/main/java/com/xunmao/demo/controller/HelloInterfaceController.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
-public class HelloController implements Controller {
+public class HelloInterfaceController implements Controller {
 
     @Override
     public ModelAndView handleRequest(HttpServletRequest req, HttpServletResponse resp) throws Exception {
