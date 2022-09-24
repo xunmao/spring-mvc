7d9a3a00202f11cd37ad3edc783ec14184ff9ebd
xunmao <xunmao1989@gmail.com>
2022-09-24 22:08:42 +0800

    优化代码

--- src/test/java/com/xunmao/demo/dao/ActorDaoTest.java
+++ src/test/java/com/xunmao/demo/dao/ActorDaoTest.java
@@ -1,7 +1,6 @@
 package com.xunmao.demo.dao;
 
 import org.junit.Test;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.xunmao.demo.dao.impl.ActorDaoImpl;
@@ -11,9 +10,12 @@ public class ActorDaoTest {
 
     @Test
     public void shouldFindActorById() {
-        ApplicationContext context = new ClassPathXmlApplicationContext("app-context.xml");
-        ActorDao actorDao = context.getBean("actorDao", ActorDaoImpl.class);
-        Actor actor = actorDao.findActorById(1);
-        System.out.println(actor);
+        try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("app-context.xml")) {
+            ActorDao actorDao = context.getBean("actorDao", ActorDaoImpl.class);
+            Actor actor = actorDao.findActorById(1);
+            System.out.println(actor);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/test/java/com/xunmao/demo/service/ActorServiceTest.java
+++ src/test/java/com/xunmao/demo/service/ActorServiceTest.java
@@ -1,7 +1,6 @@
 package com.xunmao.demo.service;
 
 import org.junit.Test;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.xunmao.demo.pojo.Actor;
@@ -11,9 +10,12 @@ public class ActorServiceTest {
 
     @Test
     public void shouldFindActorById() {
-        ApplicationContext context = new ClassPathXmlApplicationContext("app-context.xml");
-        ActorService actorService = context.getBean("actorService", ActorServiceImpl.class);
-        Actor actor = actorService.findActorById(1);
-        System.out.println(actor);
+        try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("app-context.xml")) {
+            ActorService actorService = context.getBean("actorService", ActorServiceImpl.class);
+            Actor actor = actorService.findActorById(1);
+            System.out.println(actor);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
